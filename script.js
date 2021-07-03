cb3a95afa0d05f315f7c15ad35bd8e4c52c2a9f4
danlagos <lagos.d@gmail.com>
2021-07-03 15:27:34 -0700

    open modal

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+// testing
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
