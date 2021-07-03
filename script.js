b933dc753856b8d42367344dcd927479f0e13778
danlagos <lagos.d@gmail.com>
2021-07-03 15:37:03 -0700

    functions to open and close modals

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-// testing
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
-// close window
-btnCloseModal.addEventListener('click', function () {
+// functions
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// testing
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// close window
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
